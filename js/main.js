0c2932a42c89b019e91ba94ea79e5e75426be2ae
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-30 07:50:57 -0600

    data.json file added

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@
 //
 // document.getElementById('message').innerHTML = myObj.city;
 
-var theData2 = '{"Jane":{"age":"29", "degree":{"AAS":"VMI", "BA":"UVA"}}, "Jim":{"age":"49", "degree":"MA"}}';
-var myObj = JSON.parse(theData2);
-console.log(myObj);
-
-document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
+// var theData2 = '{"Jane":{"age":"29", "degree":{"AAS":"VMI", "BA":"UVA"}}, "Jim":{"age":"49", "degree":"MA"}}';
+// var myObj = JSON.parse(theData2);
+// console.log(myObj);
+//
+// document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
