4ae1b89f388eb8893be6ef03332c52c3eeaef79d
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-30 07:48:26 -0600

    First 2 examples completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-var start = '{"favColor":"green", "favSeason":"Harvest"}';
+// var start = '{"favColor":"green", "favSeason":"Harvest"}';
+//
+// var myObject = JSON.parse(start);
+// console.log(myObject);
+//
+// var myString = JSON.stringify(myObject);
+// console.log(myString);
+//
+// var myObject = JSON.parse(myString);
+// console.log(myObject);
 
-var myObject = JSON.parse(start);
-console.log(myObject);
+// var theData1 = '{"first":"Patrick", "last":"Logan", "city":"Salt Lake City"}';
+// var myObj = JSON.parse(theData1);
+// console.log(myObj);
+//
+// document.getElementById('message').innerHTML = myObj.city;
+
+var theData2 = '{"Jane":{"age":"29", "degree":{"AAS":"VMI", "BA":"UVA"}}, "Jim":{"age":"49", "degree":"MA"}}';
+var myObj = JSON.parse(theData2);
+console.log(myObj);
+
+document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
