8383fb234f42483bfda2b22d6a93e754cedafab0
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-01 07:26:14 -0600

    working with Arrays worked

--- js/main.js
+++ js/main.js
@@ -11,29 +11,77 @@
 
 // var theData1 = '{"first":"Patrick", "last":"Logan", "city":"Salt Lake City"}';
 // var myObj = JSON.parse(theData1);
-// console.log(myObj);
 //
-// document.getElementById('message').innerHTML = myObj.city;
+//var Data3 = '{"food":"pizza", "dessert":"gelato"}'
+//var myObj = JSON.parse(Data3);
+//// console.log(myObj);
+//
+//document.getElementById('message').innerHTML = myObj.dessert;
 
 // var theData2 = '{"Jane":{"age":"29", "degree":{"AAS":"VMI", "BA":"UVA"}}, "Jim":{"age":"49", "degree":"MA"}}';
 // var myObj = JSON.parse(theData2);
-// console.log(myObj);
 //
-// document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
+////console.log(myObj)
+//// document.getElementById('message').innerHTML = myObj.Jim.degree;
+////
+//var xhr = new XMLHttpRequest();
+//
+//xhr.open('GET', 'data.json', true);
+//
+//xhr.responstType = 'text';
+//
+//
+////xhr.onreadystatechange = function() {
+////    console.log(xhr.readyState);
+////    console.log(xhr.status);
+////    console.log(xhr.statusText);
+////}
+//
+//xhr.onload = function() {
+//    if (xhr.status === 200){
+//        var myStuff = JSON.parse(xhr.responseText);
+//        console.log(myStuff);
+//    }
+//}
+//
+//xhr.send();
+
 
 var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'data.json', true);
-
+xhr.open('GET',"data.json", true);
+xhr.responseType = 'text';
 xhr.send();
 
-xhr.onreadystatechange = function() {
-    console.log(xhr.readyState);
-    console.log(xhr.status);
-    console.log(xhr.statusText);
-}
-
 xhr.onload = function() {
-  var myStuff = JSON.parse(xhr.responseText);
-  console.log(myStuff);
+    if(xhr.status === 200) {
+        var myStuff = JSON.parse(xhr.responseText);
+        console.log(myStuff);
+        
+        for (i=0; i<myStuff.presidents.length; i++) {
+            console.log(myStuff.presidents[i].first);
+            console.log(myStuff.presidents[i].last);
+            console.log(myStuff.presidents[i].served);
+        
+            console.log(myStuff.vicepresidents[i].first);
+            console.log(myStuff.vicepresidents[i].last);
+        }
+        
+        var myString = "";
+        for (i=0; i<myStuff.presidents.length; i++) {
+            var x = i+1;
+            myString += "<br>President " +x+ " was ";
+            myString += myStuff.presidents[i].first + " ";
+            myString += myStuff.presidents[i].last + ".  ";
+            
+            myString += "He served from "+ myStuff.presidents[i].served +" with " + myStuff.vicepresidents[i].first + " " + myStuff.vicepresidents[i].last +".  ";
+            
+        }
+        document.getElementById('message').innerHTML = myString;
+    }
 }
+//    console.log(myStuff);
+//    
+//    console.log(myStuff[1]);
+//    
+//    console.log(myStuff[0].first)
+//    
